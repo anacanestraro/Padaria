2ce62e206804a3598eea90e81ef6738ac191800a
anacanestraro <ana.canestraro07@gmail.com>
2024-09-03 08:55:57 -0300

    teste

---
+++ listarClientes.php
@@ -0,0 +1,53 @@
+<?php
+
+    require("Banco.php");
+    require("Cliente.php");
+
+    $banco = new Banco();
+    $conexao = $banco->getConexao();
+
+    $cliente = new Cliente($conexao);
+    $stmt = $cliente->read();
+    $clientes = $stmt->fethcAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clientes</title>
+</head>
+<body>
+    <h2>Lista de Clientes</h2>
+
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>CPF</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach($clientes as $cliente){ ?>
+            <tr>
+                <td><?php echo $cliente['id']; ?></td>
+                <td><?php echo $cliente['nome']; ?></td>
+                <td><?php echo $cliente['telefone']; ?></td>
+                <td><?php echo $cliente['email']; ?></td>
+                <td><?php echo $cliente['cpf']; ?></td>
+
+                <td>
+                    <a href="form_atualizaCliente.php?id=<?php echo $cliente['id'];?>">Editar</a>
+                    <a href="deletarCliente.php?id=<?php echo $cliente['id'];?>">Excluir</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+
+    <a href="form cadastroCliente.php">Cadastrar Novo Cliente</a>
+    
+</body>
+</html>
