2fcf16f7ea2982178a075605ca2f7453afd486b4
anacanestraro <ana.canestraro07@gmail.com>
2024-09-07 22:47:19 -0300

    teste

--- form_cadastroCliente.php
+++ form_cadastroCliente.php
@@ -5,16 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Cadastro</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css" />
 </head>
 <body>
-    <h3>Cadastro de Cliente</h3>
-        <form method="POST" action="cadastroCliente.php">
-            <p>Nome:<input type="text" name="nome" required> </p>
-            <p>Telefone:<input type="text" name="telefone" required> </p>
-            <p>Email:<input type="email" name="email" required> </p>
-            <p>CPF:<input type="text" name="cpf" required> </p>
-            <p> <button type="submit">Cadastrar</button>
-    </form>
+    <div class="d-flex vh-100 justify-content-center align-items-center">
+        <div class="container">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h3>Cadastro de Cliente</h3>
+                </div>
+                <div class="card-body">
+            <form method="POST" action="cadastroCliente.php">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="nome" name="nome" required>
+                </div>
+                <div class="mb-3">
+                    <label for="telefone" class="form-label">Telefone</label>
+                    <input type="text" class="form-control" id="telefone" name="telefone" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="cpf" class="form-label">CPF</label>
+                    <input type="text" class="form-control" id="cpf" name="cpf" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Cadastrar</button>
+                </div>
+            </form>
+        </div>                      
+            </div>
+        </div>
+    </div>
 </body>
 </html>
  
